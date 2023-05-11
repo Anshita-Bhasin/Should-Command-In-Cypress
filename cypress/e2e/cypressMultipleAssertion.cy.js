5b84d47a158bf6d6585800c4b03d0037c1b09589
Anshita Bhasin <anshita.bhasin@gmail.com>
2023-05-11 10:45:27 +0400

    update assertions

--- cypress/e2e/cypressMultipleAssertion.cy.js
+++ cypress/e2e/cypressMultipleAssertion.cy.js
@@ -4,8 +4,13 @@ describe('Cypress Assertions', () => {
         cy.visit('https://ecommerce-playground.lambdatest.io/')
         cy.get('.figure.img-top').should('have.length', 8)
             .last()
-            .should('include.text', "MP3 Players")
+            .should('include.text', "MP3 Players").should('have.css', 'font-family')
         cy.get('input[name="search"]').first().should('have.attr', 'aria-label', "Search For Products").type('iphone').should('have.value', 'iphone')
 
+    })
+    it('Should - Multiple Assertion for Dropdown', () => {
+        cy.visit('https://www.lambdatest.com/selenium-playground/select-dropdown-demo')
+        cy.get('#select-demo').select('Wednesday')
+        cy.get('#select-demo option:selected').should('be.selected').and('have.value', 'Wednesday')
     })
 })
--- cypress/e2e/cypressValueAssertions.cy.js
+++ cypress/e2e/cypressValueAssertions.cy.js
@@ -1,9 +1,12 @@
 describe('Cypress Assertions', () => {
 
-    it.only('Should - Value Assertion for Textbox', () => {
+    it('Should - Value Assertion for Textbox', () => {
         cy.visit('https://ecommerce-playground.lambdatest.io/')
         cy.get('input[name="search"]').first().type('iphone')
         cy.get('input[name="search"]').first().should('have.value', 'iphone')
+        cy.get('input[name="search"]').first().should('not.have.value', 'IPHONE')
+
+
 
     })
 
@@ -11,5 +14,6 @@ describe('Cypress Assertions', () => {
         cy.visit('https://www.lambdatest.com/selenium-playground/select-dropdown-demo')
         cy.get('#select-demo').select('Wednesday')
         cy.get('#select-demo option:selected').should('have.value', 'Wednesday')
+        cy.get('#select-demo option:selected').should('be.selected', 'Wednesday')
     })
 })
